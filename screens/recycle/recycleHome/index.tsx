837aacf6f05c90a2c244b6901af0b458c9c064a4
Daryl Wong <youcanfinddaryl@gmail.com>
2022-07-10 02:18:32 +0800

    add search bar

--- screens/recycle/recycleHome/index.tsx
+++ screens/recycle/recycleHome/index.tsx
@@ -1,5 +1,6 @@
 import { Text } from "react-native";
 import { Container } from "../../../common/components";
+import { SearchBar } from "../../../common/components/searchBar";
 // import { TScreenProp } from "../../types";
 import Categories from "./components/categories";
 import SuggestedArticles from "./components/suggestedArticles";
@@ -7,6 +8,7 @@ import SuggestedArticles from "./components/suggestedArticles";
 const RecycleHome = ({navigation}: any) => {
     return (
         <Container scrollable>
+            <SearchBar searchContainerStyle={{marginTop: 20}} />
             <Categories />
             <SuggestedArticles navigation={navigation}/>
         </Container>
